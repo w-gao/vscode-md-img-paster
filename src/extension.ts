4dd58b0c3fccaa1408284b33953270995ab359ce
William Gao <williamgao520@gmail.com>
2022-12-18 11:27:45 -0800

    Add basic logic for extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,139 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+
+const generateRandomString = (length: number): string => {
+	return Array(length).fill("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz").map(x => x[Math.floor(Math.random() * x.length)]).join('');
+};
+
+
+const promptForFilename = (defaultName: string): Promise<string> => {
+	return new Promise<string>((resolve, reject) => {
+		vscode.window.showInputBox({
+			prompt: 'Please specify the filename of the image.',
+			value: defaultName
+		}).then((value?: string) => {
+			if (!value) {
+				reject("you entered an empty filename");
+				return;
+			}
+
+			const filename = value.trim();
+
+			// It looks like MacOS only disallows the path separator character in a filename, but we should switch to
+			// regex if we want to prohibit more characters.
+			if (filename.indexOf("/") >= 0) {
+				reject("invalid filename (cannot contain \"/\")");
+				return;
+			}
+
+			resolve(filename);
+		});
+	});
+};
+
+
+/**
+ * Paste image from clipboard.
+ *
+ * @param folder The path under root in which the image should be stored.
+ * @param defaultName The default name of the image. This could be replaced by the user.
+ * @returns 
+ */
+const pasteImage = (folder: string, defaultName: string) => {
+
+	if(vscode.workspace.workspaceFolders === undefined) {
+		vscode.window.showInformationMessage("No workspace opened!");
+		return;
+	}
+
+	const rootPath = vscode.workspace.workspaceFolders[0].uri.path;
+	console.debug(`rootPath=${rootPath}`);
+
+	const textEditor = vscode.window.activeTextEditor;
+	if (!textEditor) {
+		vscode.window.showInformationMessage("Please select a document first.");
+		return;
+	}
+
+	if (textEditor.document.isUntitled) {
+		vscode.window.showInformationMessage("Please save the document so we can get the path.");
+		return;
+	}
+
+	// Get the file URI
+	const uri = textEditor.document.fileName;
+	const dirname = path.dirname(uri);
+	// const relPath = path.relative(dirname, path.join(rootPath, "images", "img_test.png"));
+
+	// If file does not seem to be markdown, ask the user if they want to continue.
+	new Promise((resolve, reject) => {
+		if (!(uri.endsWith(".markdown") || uri.endsWith(".md"))) {
+			vscode.window.showInformationMessage("The file extension does not end in .md or .markdown. Do you want to continue?", "Continue", "Cancel")
+				.then((value) => {
+					if (value === "Continue") {
+						resolve(null);
+						return;
+					}
+					reject(null);  // no need to log error
+				});
+			return;
+		}
+
+		resolve(null);
+	})
+	// Great, now ask for a filename.
+	.then(() => {
+		return promptForFilename(defaultName);
+	})
+
+	.then(filename => {
+		const imgPath = path.join(rootPath, folder, filename);
+
+		// Make sure this file does not exist.
+		const exists = true;
+
+		if (exists) {
+			// vscode.window.showInformationMessage(`${filename} already exists.`, "Enter new name", "Replace", "Cancel")
+			// 	.then(value => { });
+			throw new Error("duplcate filename");
+		}
+
+		// Pass it on.
+		return filename;
+	})
+
+	.then((filename) => {
+		vscode.window.showInformationMessage(filename);
+	})
+
+	// Uh oh, something went wrong.
+	.catch(reason => {
+		if (reason) {
+			if (reason instanceof Error) { reason = reason.message; }
+			vscode.window.showErrorMessage(`Failed to paste image: ${reason}.`);
+		}
+	});
+
+	return;
+};
+
 export function activate(context: vscode.ExtensionContext) {
+	console.log("vscode-md-img-paster triggered!");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-md-img-paster" is now active!');
+	let disposable = vscode.commands.registerCommand('vscode-md-img-paster.pasteImg', () => {
+		let platform = process.platform;
+		if (platform !== "darwin") {
+			vscode.window.showErrorMessage(`md-img-paster does not work on ${platform}.`);
+			return;
+		}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-md-img-paster.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Markdown Image Paster!');
+		pasteImage("images", `img_${generateRandomString(6)}.png`);
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+
+}
